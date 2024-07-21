42b4c7812063a87d9c2657471f73f559695e5249
YehyunLee <glad7cu@gmail.com>
2024-07-21 18:09:54 -0400

    07 21

---
+++ 1 TwoIntegerSum.py
@@ -0,0 +1,16 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    # for i in range(0, len(nums)):
+    #     for j in range(i + 1, len(nums)):
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
+
+    prevMap = {}  # value:index
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9))  # [0, 1]
---
+++ 2 ProductsOfArrayDiscludingSelf.py
@@ -0,0 +1,33 @@
+def productExceptSelf(nums: list[int]) -> list[int]:
+    # final = [1] * len(nums)  # len nums = 5; final len 5 [1, 1, ..]
+    # for number in range(0, len(nums)):
+    #     for others in range(0, len(nums)):
+    #         if number != others:
+    #             final[number] *= nums[others]
+    # return final
+
+
+    # final = [1] * len(nums)
+    # for i in range(len(nums)):
+    #     prefix = 1
+    #     for j in range(0, i):
+    #         prefix *= nums[j]
+    #     postfix = 1
+    #     for k in range(i + 1, len(nums)):
+    #         postfix *= nums[k]
+    #     final[i] = prefix * postfix
+    # return final
+
+
+    final = [1] * (len(nums))
+    prefix = 1
+    for i in range(len(nums)):
+        final[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        final[i] *= postfix
+        postfix *= nums[i]
+    return final
+
+print(productExceptSelf([1, 2, 3, 4]))  # [24, 12, 8, 6]
