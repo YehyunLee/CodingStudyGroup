daefbfff11cfd72a4cdc1255f7c519d29801916d
YehyunLee <glad7cu@gmail.com>
2024-09-14 22:42:28 -0400

    August 25.

--- 2 Two Pointers/11 maxArea.py
+++ 2 Two Pointers/11 maxArea.py
@@ -1,5 +1,16 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
+        """
+        Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+        n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+        Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+        Notice that you may not slant the container.
+        Example:
+        Input: height = [1,7,2,5,4,7,3,6]
+        Output: 36
+        :param heights:
+        :return:
+        """
         l, r = 0, len(heights) - 1
         res = 0
 
